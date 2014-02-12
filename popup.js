423fedeb103ffa0793a0fe3f9050b57b5c051477
David Ackerson <david.ackerson2@gmail.com>
2014-02-11 20:36:55 -0400

    Url is auto-filled, title is almost auto-filled

--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
 function OpenManager() {
   chrome.tabs.create({url:"manager.html"})
 }
-document.getElementById("managerBtn").onclick=OpenManager
+document.getElementById("managerBtn").onclick = OpenManager
+document.getElementById("newEntryTitle").value = document.title
+document.getElementById("newEntryUrl").value = document.URL

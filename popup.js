fb875d9bbbb8a71f8da1dc89f29b95b1486158aa
David Ackerson <david.ackerson2@gmail.com>
2014-02-26 20:38:59 -0400

    Including semicolons at the end of statements

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 function OpenManager() {
-  chrome.tabs.create({url:"manager.html"})
+  chrome.tabs.create({url:"manager.html"});
 }
-document.getElementById("managerBtn").onclick = OpenManager
+document.getElementById("managerBtn").onclick = OpenManager;
 chrome.tabs.getSelected(function(tab){
-  document.getElementById("newEntryTitle").value = tab.title
-  document.getElementById("newEntryUrl").value = tab.url
+  document.getElementById("newEntryTitle").value = tab.title;
+  document.getElementById("newEntryUrl").value = tab.url;
 })

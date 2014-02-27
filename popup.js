2ca3d0e1fea6fe1176e594ddf1f08e5bedb28158
David Ackerson <david.ackerson2@gmail.com>
2014-02-26 20:43:10 -0400

    Added onclick function to newEntrySubmit button

--- popup.js
+++ popup.js
@@ -6,3 +6,14 @@ chrome.tabs.getSelected(function(tab){
   document.getElementById("newEntryTitle").value = tab.title;
   document.getElementById("newEntryUrl").value = tab.url;
 })
+document.getElementById("newEntrySubmit").onclick = function(){
+  if (localStorage["entries"] == undefined) {
+    localStorage["entries"] = [];
+  }
+  url = document.getElementById("newEntryUrl").value;
+  title = document.getElementById("newEntryTitle").value;
+  localStorage["entries"] = {}
+  localStorage["entries"][url] = {
+    "title": title
+  };
+};

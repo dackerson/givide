0fe390fbf6ea4e3ab6958350c583c7bf63428de0
David Ackerson <david.ackerson2@gmail.com>
2014-04-06 22:25:23 -0300

    Cleaning up

--- popup.js
+++ popup.js
@@ -1,22 +1,23 @@
 function OpenManager() {
   chrome.tabs.create({url:"manager.html"});
-}
+};
 document.getElementById("managerBtn").onclick = OpenManager;
 chrome.tabs.getSelected(function(tab){
   document.getElementById("newEntryTitle").value = tab.title;
   document.getElementById("newEntryUrl").value = tab.url;
-})
+});
 document.getElementById("newEntrySubmit").onclick = function(){
   url = document.getElementById("newEntryUrl").value;
   title = document.getElementById("newEntryTitle").value;
-  entriesStr = localStorage["entries"]
+  entriesStr = localStorage["entries"];
   if (entriesStr == undefined) {
     entries = {};
   }
   else {
     entries = JSON.parse(entriesStr);
   }
-  console.log(JSON.stringify(entries));
   entries[url] = title;
-  localStorage["entries"] = JSON.stringify(entries);
+  entriesStr = JSON.stringify(entries);
+  console.log("Debug -- new value of 'entries' is: " + entriesStr);
+  localStorage["entries"] = entriesStr;
 };
